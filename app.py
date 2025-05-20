76fdcf7ca8907077efe9be5a03ef51a91a31cf92
kaykim0310 <kim.kangyoon@gmail.com>
2025-05-20 15:17:44 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="근골격계 증상조사 설문시스템", layout="centered")
+
+st.title("근골격계 증상조사 설문시스템")
+st.markdown("---")
+st.success("앱이 정상 작동 중입니다. 설문 내용은 다음 단계에서 시작됩니다.")
